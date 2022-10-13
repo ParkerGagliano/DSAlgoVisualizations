4353d8e6204345eb31a0be606e25b31147324000
parker gagliano <pjg4086@uncw.edu>
2022-10-13 13:02:54 -0400

    cleaned up code, working visual

--- Components/script.js
+++ Components/script.js
@@ -4,7 +4,6 @@ let leftarrow = document.createElement('div')
 let rightarrow = document.createElement('div')
 
 
-console.log(container)
 class QueueSite{
     constructor(){  
         this.queue = null;
@@ -29,16 +28,31 @@ class QueueSite{
             this.flashColor(leftarrow, 'danger');
         }
     }
+
+    createEL(name, attr, intext) {  // Element maker 
+        let temp = document.createElement(name)
+        if (attr) {
+            temp.classList.add(attr)
+        }
+        if (intext) {
+            temp.innerText=intext
+        }
+        return temp
+       
+
+    }
+
     render(){
         container.innerHTML = ''
         leftarrow.innerHTML = ''
+        rightarrow.innerHTML = ''
         leftarrow.classList.add('col-1')
-        let insert = document.createElement('h5')
-        insert.innerText='Remove'
-        leftarrow.appendChild(insert)
+        let del = this.createEL('h5', null, 'Del')
+        leftarrow.appendChild(del)
         container.appendChild(leftarrow)
         rightarrow.classList.add('col-1')
-        rightarrow.innerHTML = '(=='
+        let insert = this.createEL('h5', null, 'Insert')
+        rightarrow.appendChild(insert)
         let temparr = this.queue.fullArray;
         for (let i = 0; i < temparr.length; i++) {
             let color = 'secondary'
@@ -47,9 +61,7 @@ class QueueSite{
                 color = 'muted'
             }
             col.className = `col-1 border bg-${color}`;
-            let value = document.createElement('h5');
-            value.innerHTML=temparr[i]
-            value.classList = 'text-center';
+            let value = this.createEL('h5', 'text-center', temparr[i])
             col.appendChild(value)
             container.appendChild(col);
           }
